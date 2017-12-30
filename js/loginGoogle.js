b81daec1c76360b35e9eef8155e64ee12a253e06
Jarrickvancamp <jarrickvancamp@gmail.com>
2017-12-31 00:42:23 +0100

    Google authentication

---
+++ js/loginGoogle.js
@@ -0,0 +1,9 @@
+
+//Google Login, fetch basic profile
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId());
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
+}
