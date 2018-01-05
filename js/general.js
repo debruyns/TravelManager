f3088f32f85f315a6fead65c6f936792592d2bec
Sam Debruyn <sam@citytakeoff.com>
2018-01-05 01:46:52 +0100

    Page Design

--- js/general.js
+++ js/general.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 
-
-
+  // Show menu when clicking on the menu selector
   $("#menu-sel").click(function(){
 
     if ($("#menu-list").is(":visible")){
@@ -14,6 +13,21 @@ $(document).ready(function(){
 
   });
 
+  // Hide menu when clicking outside of it
+  $(document).mouseup(function(e){
+
+    var container = $("#menu-list");
+
+    if ($("#menu-list").is(":visible")){
+
+      if (!container.is(e.target) && container.has(e.target).length === 0){
+        container.slideUp();
+      }
+    }
+
+  });
+
+  // Function to rotate
   jQuery.fn.rotate = function(degrees) {
     $(this).css({'transform' : 'rotate('+ degrees +'deg)'});
     return $(this);
