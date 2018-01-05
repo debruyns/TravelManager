e30c6bac7620a58eb2bc6e6ea4be1d253348ad5a
Sam Debruyn <sam@citytakeoff.com>
2018-01-05 15:17:33 +0100

    Account Design

--- account.php
+++ account.php
@@ -3,12 +3,12 @@
 
 <h1 class="account-title"><?= $i18n['ACCOUNT_TITLE']; ?></h1>
 
-<table class="account-settings">
-  <tr>
-    <td><?= $i18n['ACCOUNT_PROFILE']; ?></td>
-    <td><button><?= $i18n['ACCOUNT_EDIT']; ?></button></td>
-  </tr>
-</table>
+<div class="all-settings">
+  <div class="account-setting">
+    <div class="setting-name"><?= $i18n['ACCOUNT_PROFILE']; ?></div>
+    <div class="setting-action"><button><?= $i18n['ACCOUNT_EDIT']; ?></button></div>
+  </div>
+</div>
 
 <!-- Include template footer -->
 <?php include_once('includes/template/footer.tpl.php'); ?>
