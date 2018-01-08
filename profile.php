a5499d1d7b37b034da6dddb8faf0ba004393e2cb
Sam Debruyn <sam@citytakeoff.com>
2018-01-08 22:12:53 +0100

    Error & Success message setup

--- profile.php
+++ profile.php
@@ -4,6 +4,8 @@
 <h1 class="page-title"><?= $i18n['PROFILE_TITLE']; ?></h1>
 
 <div class="form-container">
+  <div id="error-message" class="error-message"></div>
+  <div id="success-message" class="success-message"></div>
   <div class="form-element">
       <label><?= $i18n['LABEL_FIRSTNAME']; ?></label>
       <input type="text" id="profile_firstname" placeholder="<?= $i18n['PLACEHOLDER_FIRSTNAME']; ?>" value="<?= $auth_user->getFirstname(); ?>" />
@@ -16,7 +18,7 @@
       <label><?= $i18n['LABEL_EMAIL']; ?></label>
       <input type="email" id="profile_email" placeholder="<?= $i18n['PLACEHOLDER_EMAIL']; ?>" value="<?= $auth_user->getEmail(); ?>" />
   </div>
-  <button class="save"><?= $i18n['FORM_SAVE_CHANGES']; ?></button>
+  <button class="save" id="profile_button"><?= $i18n['FORM_SAVE_CHANGES']; ?></button>
   <a href="/account" class="cancel"><?= $i18n['FORM_CANCEL']; ?></a>
 </div>
 
