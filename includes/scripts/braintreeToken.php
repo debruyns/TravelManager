bff1d8e8210c2f8dcdad7b753f4536d01544cbed
Sam Debruyn <sam@citytakeoff.com>
2018-01-14 23:05:26 +0100

    Premium

--- includes/scripts/braintreeToken.php
+++ includes/scripts/braintreeToken.php
@@ -10,4 +10,6 @@ Braintree_Configuration::merchantId('mbc2kjh4pdqf3xht');
 Braintree_Configuration::publicKey('vcgk7j7tpzrp5238');
 Braintree_Configuration::privateKey('a8c8a20106cbf85a269595ab936d2286');
 
+echo Braintree_ClientToken::generate();
+
 ?>
