88a60630de958e64926f70930be5480648345eba
Sam Debruyn <sam@citytakeoff.com>
2018-01-14 22:55:18 +0100

    Premium

--- includes/scripts/braintreeToken.php
+++ includes/scripts/braintreeToken.php
@@ -3,11 +3,11 @@
 // Root location
 $path = $_SERVER['DOCUMENT_ROOT'];
 
-require_once($path . "/includes/payments/Braintree.php");
+require_once($path . "/includes/payments/lib/Braintree.php");
 
-Braintree_Configuration::environment('sandbox');
+/*Braintree_Configuration::environment('sandbox');
 Braintree_Configuration::merchantId('mbc2kjh4pdqf3xht');
 Braintree_Configuration::publicKey('vcgk7j7tpzrp5238');
-Braintree_Configuration::privateKey('a8c8a20106cbf85a269595ab936d2286');
+Braintree_Configuration::privateKey('a8c8a20106cbf85a269595ab936d2286');*/
 
 ?>
